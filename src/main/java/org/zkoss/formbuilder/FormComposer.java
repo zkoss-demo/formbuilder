01a949c5f33a0e6ee5ad26cea6d70a22ea46670f
hawkchen <hawkchen@potix.com>
2025-02-12 20:16:00 +0800

    default FormComposer sends an event to the default event queue

--- src/main/java/org/zkoss/formbuilder/FormComposer.java
+++ src/main/java/org/zkoss/formbuilder/FormComposer.java
@@ -2,7 +2,9 @@ package org.zkoss.formbuilder;
 
 import java.util.*;
 
+import com.google.gson.JsonObject;
 import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.*;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.*;
 import org.zkoss.zk.ui.util.*;
@@ -14,15 +16,19 @@ import org.zkoss.zul.impl.InputElement;
  * default composer for the generated form
  */
 public class FormComposer extends SelectorComposer {
+	public static String RESULT_KEY = "formResult";
 
 	@Wire("textbox, intbox")
-	List<InputElement> inputElements;
+	protected List<InputElement> inputElements;
+	public static String EVENT_QUEUE_NAME = "formEvent";
+	public static String EVENT_SAVE = "onSaveForm";
+	protected EventQueue eventQueue = EventQueues.lookup(EVENT_QUEUE_NAME, true); //desktop-level
 
 	@Listen("onClick=#savebtn")
 	public void doSaveForm() {
-		Map<String, Object> returnedValues = new HashMap<String, Object>();
+		JsonObject result = new JsonObject();
 		for (Iterator<InputElement> iterator = inputElements.iterator(); iterator.hasNext();) {
-			Component comp = iterator.next();
+			InputElement comp = iterator.next();
 			Object value = null;
 			switch (comp.getClass().getSimpleName()) {
 			case "Textbox":
@@ -34,8 +40,13 @@ public class FormComposer extends SelectorComposer {
 			default:
 				break;
 			}
-			returnedValues.put(comp.getId(), value);
+			result.addProperty(comp.getId(), value.toString());
 		}
-		Notification.show(returnedValues.toString());
+		getPage().getDesktop().setAttribute(RESULT_KEY, result.toString());
+		eventQueue.publish(createSaveEvent(result.toString()));
+	}
+
+	protected Event createSaveEvent(String result){
+		return new Event(EVENT_SAVE, null, result);
 	}
 }
